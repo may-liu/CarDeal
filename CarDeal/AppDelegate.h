3a22feca8b52ffdfc436b452ba17a18e758a3bed
May <May@MaytekiMacBook-Pro.local>
2014-01-02 13:12:37 +0800

    Initial Commit

---
+++ CarDeal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CarDeal
+//
+//  Created by May on 14-1-2.
+//  Copyright (c) 2014年 lvtu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CarDeal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CarDeal
+//
+//  Created by May on 14-1-2.
+//  Copyright (c) 2014年 lvtu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
