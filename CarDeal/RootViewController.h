19a74ac52a566ab5689f75fecab0d6a9400b9e59
May <May@MaytekiMacBook-Pro.local>
2014-01-02 13:15:18 +0800

    asdfvv

---
+++ CarDeal/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  CarDeal
+//
+//  Created by May on 14-1-2.
+//  Copyright (c) 2014年 lvtu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UINavigationController
+
+@end
---
+++ CarDeal/RootViewController.m
@@ -0,0 +1,43 @@
+//
+//  RootViewController.m
+//  CarDeal
+//
+//  Created by May on 14-1-2.
+//  Copyright (c) 2014年 lvtu. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
